c4621ea4dfe88ccbd89d9cc4a5515314fedd563f
jinx <550961784@qq.com>
2022-04-08 14:21:20 +0800

    feat: 添加 react-app-rewired, 以便后期扩展

---
+++ config-overrides.js
@@ -0,0 +1,17 @@
+/* eslint-disable global-require */
+const { override } = require('customize-cra')
+
+// fix
+// https://github.com/formatjs/formatjs/issues/1395#issuecomment-518823361
+const supportMjs = () => (webpackConfig) => {
+  webpackConfig.module.rules.push({
+    test: /\.mjs$/,
+    include: /node_modules/,
+    type: 'javascript/auto'
+  })
+  return webpackConfig
+}
+
+module.exports = {
+  webpack: override(supportMjs())
+}
